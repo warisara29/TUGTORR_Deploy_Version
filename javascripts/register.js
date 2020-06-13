5619d1b8a406207817fd69fefa561632b8d8d062
warisara29 <p_warisara@kkumail.com>
2020-06-14 00:46:58 +0700

    update

---
+++ javascripts/register.js
@@ -0,0 +1,32 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBTsuGF9QEwGQDM1F_4nn2EpMgh5maLNWQ",
+    authDomain: "tugtorr-691a5.firebaseapp.com",
+    databaseURL: "https://tugtorr-691a5.firebaseio.com",
+    projectId: "tugtorr-691a5",
+    storageBucket: "tugtorr-691a5.appspot.com",
+    messagingSenderId: "1048933698438",
+    appId: "1:1048933698438:web:4476a50309f38b4af6fa07"
+};
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// sign up function
+var signUp = document.getElementById("btn-register");
+
+signUp.addEventListener("click", function(event) {
+    event.preventDefault();
+    var email = document.getElementById("email").value
+    var password = document.getElementById("password").value
+
+    
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+    });
+
+    console.log(email)
+    console.log(password)
+})
