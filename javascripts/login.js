4f609674226e8f29d60c2606a3cfa0c55a4d8fb4
warisara29 <p_warisara@kkumail.com>
2020-06-14 00:07:11 +0700

    update

--- javascripts/login.js
+++
@@ -1,9 +0,0 @@
-var signup = document.getElementById("signup")
-signup.onclick = function () {
-    location.href = "register"
-}
-
-var login = document.getElementById("login")
-login.onclick = function () {
-    location.href = "index"
-}
