5e1a620b4b5676b10e9a34a637154c268bee9519
warisara29 <p_warisara@kkumail.com>
2020-06-15 23:19:27 +0700

    update

--- javascripts/login.js
+++ javascripts/login.js
@@ -38,19 +38,36 @@ login.addEventListener("click", function(event) {
 
 function google_login() {
     var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    firebase.auth().useDeviceLanguage();
+    provider.setCustomParameters({
+        'login_hint': 'user@example.com'
+    });
     firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
         var token = result.credential.accessToken;
+        // The signed-in user info.
         var user = result.user;
+        // ...
+        alert('SUCCESS!!')
+        console.log('token is ' + token)
+        console.log('user is '+ user)
 
-        console.log(token)
-        console.log(user)
-    }).catch(function(error) {
-        var errorCode = error.code
-        var errorMessage = error.message
-
+        window.location.href = "index.html"
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+        alert('ERROR!!!!')
         console.log(errorCode)
         console.log(errorMessage)
-    })
+        window.location.href = "register.html"
+      });
 }
 
 
