075ff7afb301bb81e04e1f61a5a7720c0e62972e
warisara29 <p_warisara@kkumail.com>
2020-06-15 15:30:20 +0700

    update

--- javascripts/login.js
+++ javascripts/login.js
@@ -37,27 +37,18 @@ login.addEventListener("click", function(event) {
 })
 
 //sign in with facebook
-function facebook_login() {
-    var provider = new firebase.auth.FacebookAuthProvider();
-    provider.addScope('user_birthday');
-    firebase.auth().useDeviceLanguage();
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+var provider = new firebase.auth.FacebookAuthProvider();
+function loginFacebook() {
+    firebase.auth().signInWithPopup(provider)
+    .then(function(result) {
         var token = result.credential.accessToken;
-        // The signed-in user info.
         var user = result.user;
-        console.log('user')
-        alert('login successfully')
-        // ...
-      }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
+
+        console.log(token)
+        console.log(user)
+    }).catch(function(error) {
+        console.log(error.code)
+        console.log(error.message)
+    })
 }
  
