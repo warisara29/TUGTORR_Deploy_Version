a8eff720cfb667f8e8e8d9719ddf7ee5199dfc5e
warisara29 <p_warisara@kkumail.com>
2020-06-15 23:30:01 +0700

    update

--- javascripts/login.js
+++ javascripts/login.js
@@ -70,5 +70,37 @@ function google_login() {
       });
 }
 
+function facebook_login() {
+    var provider = new firebase.auth.FacebookAuthProvider();
+    provider.addScope('user_birthday');
+    firebase.auth().useDeviceLanguage();
+    provider.setCustomParameters({
+        'display': 'popup'
+    });
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+
+        alert('SUCCESS!!')
+        console.log('token is ' + token)
+        console.log('user is '+ user)
+
+        window.location.href = "index.html"
+        
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+}
+
 
  
