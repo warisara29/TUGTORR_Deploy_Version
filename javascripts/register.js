5fee7d53a89b8e148dda0c3ba47eab3b31c1b009
warisara29 <p_warisara@kkumail.com>
2020-06-18 01:21:30 +0700

    update

--- javascripts/register.js
+++ javascripts/register.js
@@ -22,12 +22,14 @@ signUp.addEventListener("click", function(event) {
 
     if (password === confirmPassword) {
       firebase.auth().createUserWithEmailAndPassword(email, password).then( function(){
+        firebase.auth().currenUser.sendEmailVerification().then(function() {
+          alert('Email verification sent!')
+        })
         alert('sign up success!!')
         window.location.href = "login.html"
       }).catch(function(error) {
         var errorCode = error.code;
         var errorMessage = error.message;
-
         window.alert("Error : " + errorMessage)
       })
     } else {
