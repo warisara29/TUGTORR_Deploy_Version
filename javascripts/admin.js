dcf4a4051248564c60b189138ca630db7eff1b25
warisara29 <p_warisara@kkumail.com>
2020-06-18 22:37:22 +0700

    update

--- javascripts/admin.js
+++ javascripts/admin.js
@@ -18,3 +18,46 @@ signout.addEventListener("click", (e) => {
         window.location.href = 'login.html'
     })
 })
+
+//add product
+
+function popupTest() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "block";
+        }
+    }
+}
+
+function upLoad() {
+
+    //get input value
+    var name = document.getElementById("product-name").value
+    var category = document.getElementById("category").value
+    var images = document.getElementById("image").value
+    var description = document.getElementById("description").value
+    var price = document.getElementById("price").value
+
+    //push data to firebase project
+    firebase.database().ref("Storage").push( {
+        name: name,
+        category: category,
+        images: images,
+        description: description,
+        price: price
+    })
+
+    alert('New product added!')
+}
+
